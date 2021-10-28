614a5cfcc6c445bfa76f9652948e8311523bab42
sagwamas <83221049+sagwamas@users.noreply.github.com>
2021-10-28 21:04:56 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector('.next1').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(0vw)';
+})
+
+document.querySelector('.next2').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(-100vw)';
+})
+
+document.querySelector('.next3').addEventListener('click', function(){
+    document.querySelector('.container').style.transform = 'translate(-200vw)';
+})
+
